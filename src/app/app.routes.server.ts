224a8f38ddc6632dc54532c89fa68b179ed7df93
Ahmed Anan <enan611@gmail.com>
2026-08-08 11:14:54 +0300

    fix: use SSR render mode for dynamic routes, adjust bundle budget

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,22 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'product-details/:id/:slug',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'categories/:categoryId',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'categories/:categoryId/:subCategoryId/:slug',
+    renderMode: RenderMode.Server
+  },
+  {
+    path: 'brands/:id',
+    renderMode: RenderMode.Server
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender
   }
